6cbfb8e908d17bae04ffe3e2828bfbd73a7b90d6
Захарчук Кирило <kirilzaharcuk48@gmail.com>
2026-09-04 16:38:50 +0300

    Add files via upload

---
+++ Лабораторна робота #1.py
@@ -0,0 +1,49 @@
+#перша задача
+
+print ("Hello World!")
+
+#ЗМІННІ
+
+my_int = 10                  #ціле число
+my_float = 3.14              #число з крапкою
+my_str = "Камаз"             #текст
+my_bool = True               #true or false
+my_list = [1, 2, 3, 4, 5, 6] #список
+
+#ВИВІД ЗНАЧЕННЯ ЗМІННИХ
+
+print (my_int, type(my_int))
+print(my_float, type(my_float))
+print(my_str, type(my_str))
+print(my_bool, type(my_bool))
+print(my_list, type(my_list))
+
+#ОПЕРАТОРИ
+
+#беремо підставні числа
+a=3
+b=7
+
+#1.Арифметика (+,-,*,/,%,**,//)
+print(a + b)  #додати
+print(a - b)  #відняти
+print(a * b)  #помножити
+print(a / b)  #поділити
+print(a // b) #ділення без остачі
+print(a % b)  #залишок від ділення
+print(a ** b) #піднесення
+
+#2.Порівняння (>,<,>=,<=,==,!=)
+print(a > b)  #більше
+print(a < b)  #менше
+print(a >= b) #більше або дорівнює
+print(a <= b) #менше або дорівнює
+print(a == b) #дорівнює
+print(a != b) #не дорівює
+
+
+
+
+
+
+
